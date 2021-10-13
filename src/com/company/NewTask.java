de434b559e58f10e394177fbe622d62919ca02a5
ErlendBerntsen <Erlend.Berntsen@student.uib.no>
2021-10-13 17:39:13 +0200

    Initial commit

---
+++ src/com/company/NewTask.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.nio.charset.StandardCharsets;
+
+public class NewTask {
+    private final static String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args)throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()
+        ){
+            boolean durable = true;
+            channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
+            String message = String.join(" ", args);
+            channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+
+    }
+}
